21d2c1302f7d18dba38b441f5bbf91466426d5f4
moh008 <moh008@ucr.edu>
2020-06-26 21:03:10 -0700

    dfs and bfs updated

--- search.py
+++ search.py
@@ -73,25 +73,59 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
+    currentState = []
+    currentPath = []
+    visited = []
+    
+    stack = util.Stack()
+    paths = util.Stack()
+    
+    stack.push(problem.getStartState())
+    paths.push([])
 
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    "*** YOUR CODE HERE ***"
+    while stack.isEmpty() != True:
+        currentState = stack.pop()
+        currentPath = paths.pop()
+        
+        if problem.isGoalState(currentState):
+            return currentPath
+        
+        else: 
+            if currentState not in visited:
+                for i in problem.getSuccessors(currentState):
+                    stack.push(i[0])
+                    paths.push(currentPath + [i[1]])
+                visited.append(currentState)
+    return currentPath
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
+from util import Queue
+    currentState = []
+    currentPath = []
+    visited = []
+    
+    queue = util.Queue()
+    paths = util.Queue()
+    
+    queue.push(problem.getStartState())
+    paths.push([])
+    
+    while queue.isEmpty() != True:
+        currentState = queue.pop()
+        currentPath = paths.pop()
+        
+        if problem.isGoalState(currentState):
+            return currentPath
+            
+        else:
+            if currentState not in visited:
+                for i in problem.getSuccessors(currentState):
+                    queue.push(i[0])
+                    paths.push(currentPath + [i[1]])
+                visited.append(currentState)
+    
+    return currentPath
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
